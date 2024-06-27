668f09f1618abf96fd8c64dfb8f832b41c44dfe3
peter <peter@marscompany.co>
2024-06-28 02:03:25 +0900

    asdf

--- application.0508f.js
+++ application.0508f.js
@@ -97,7 +97,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.37ac1.json';
+      var settings = 'src/settings.553af.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.0397e.js
+++ index.0397e.js
@@ -1,4 +1,4 @@
-System.register(["./application.2d51b.js"], function (_export, _context) {
+System.register(["./application.0508f.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.2d51b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application2d51bJs) {
-      createApplication = _application2d51bJs.createApplication;
+    setters: [function (_application0508fJs) {
+      createApplication = _application0508fJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
